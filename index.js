5d99093c84f9c7419a370e8a3245821859bbb02e
Nikhil <nikhilpatil763@gmail.com>
2019-02-11 14:55:36 +0530

    correct spelling error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-cost GoogleStrategy = require('passport-google-oauth20').Strategy();
+const GoogleStrategy = require('passport-google-oauth20').Strategy();
 
 const app = express();
 
