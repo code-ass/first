83d64117324dbef5827286cc35905d1ac2541e55
Nikhil <nikhilpatil763@gmail.com>
2019-02-11 14:48:23 +0530

    Adding google oauth api

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+cost GoogleStrategy = require('passport-google-oauth20').Strategy();
+
 const app = express();
+
+passport.use(new GoogleStrategy());
+
+
 
 app.get('/', (req,res) => {
     res.send('Dude, Im not a Monk here');
